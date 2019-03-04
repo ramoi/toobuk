2b422dbaeccda2a68b65f4308434ade5123a5fea
moiram <ramoi@hanmail.net>
2019-03-04 13:52:57 +0900

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
  
  
 setup(name='toobuk',
-      version='0.5.1',
+      version='0.5.2',
       url='https://github.com/ramoi/toobuk',
       license='MIT',
       author='mklee',
