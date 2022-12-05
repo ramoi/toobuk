45a38d6c9751d6389b04e29c3a1ff5c1598d0bb5
moiram <ramoi@hanmail.net>
2022-12-05 20:16:06 +0900

    json load 오류 수정

--- toobuk/conf.py
+++ toobuk/conf.py
@@ -51,7 +51,8 @@ class Configure :
 	def __load__(self, path) :
 		jsonTuple = None
 		with codecs.open( path, 'r', encoding='utf-8' ) as f : 
-			jsonTuple = json.loads( f.read(), encoding="utf-8" )
+			# jsonTuple = json.loads( f.read(), encoding="utf-8" )
+			jsonTuple = json.loads( f.read() )
 
 		return jsonTuple
 
